17e7effae62bd346f620836d6abb1c92300adeef
thomnico <thomnico@googlemail.com>
2017-06-05 13:25:24 +0200

    Clean up and better handling for pypi

--- setup.py
+++ setup.py
@@ -2,13 +2,27 @@
 
 from setuptools import setup
 
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
 setup(
-      name='fortiosapi',
-      version='0.5',
-      description='Python modules to interact with fortinet products configuration rest and ssh',
-      install_requires=['requests','paramiko'],
-      author='Nicolas Thomas',
-      author_email='nthomas@fortinet.com',
-      url='https://github.com/thomnico/fortiosapi',
-      packages=['fortiosapi'],
-      )
+    name='fortiosapi',
+    version='0.5.1',
+    description='Python modules to interact with fortinet products configuration rest and ssh',
+    long_description=readme(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: Apache v2 License',
+        'Programming Language :: Python ',
+        'Topic :: Security :: Fortinet products management',
+    ],
+    keywords='Fortinet fortigate fortios rest api',
+    install_requires=['requests', 'paramiko'],
+    author='Nicolas Thomas',
+    author_email='nthomas@fortinet.com',
+    url='https://github.com/thomnico/fortiosapi',
+    include_package_data=True,
+    packages=['fortiosapi'],
+)
