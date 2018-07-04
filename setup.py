87be57eadea89498719d1d56ea56b4a953eddc31
thomnico <thomnico@googlemail.com>
2018-07-04 15:12:17 +0200

    Bump version, Add ssh port option close #

--- setup.py
+++ setup.py
@@ -10,20 +10,20 @@ def readme():
 
 setup(
     name='fortiosapi',
-    version='0.9.91',
+    version='1.0.0',
     description=('Python modules to interact with fortinet'
                  ' products configuration rest and ssh'),
     long_description=readme(),
     # Valid Classifiers are here:
     # https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python ',
         'Topic :: Security',
     ],
     keywords='Fortinet fortigate fortios rest api',
-    install_requires=['requests', 'paramiko'],
+    install_requires=['requests', 'paramiko', 'oyaml'],
     author='Nicolas Thomas',
     author_email='nthomas@fortinet.com',
     url='https://github.com/fortinet-solutions-cse/fortiosapi',
